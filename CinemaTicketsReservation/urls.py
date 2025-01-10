abf5cc8206a8ae83cb07981af2ecac7dbf0a2986
YousLam <ys5128837@gmail.com>
2025-01-10 19:38:26 +0100

    setup the rest_framework_simplejwt Configuration.

--- CinemaTicketsReservation/urls.py
+++ CinemaTicketsReservation/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('accounts.urls'))
+    path('api/', include('accounts.urls')),
+    path('api/token', TokenObtainPairView.as_view()),
 ]
