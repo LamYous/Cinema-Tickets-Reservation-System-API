967a3b9ac7f31f43bd2478e82a4f8c4ca525b4bc
YousLam <ys5128837@gmail.com>
2025-01-10 15:01:55 +0100

    Create the models and makemigrations.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hall, Movie, Seat, Showtime
 
 # Register your models here.
+
+admin.site.register(Hall)
+admin.site.register(Movie)
+admin.site.register(Seat)
+admin.site.register(Showtime)
